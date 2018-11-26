c056809d36d0fa102e55defa3b99f5534204ca97
Lostsory <2462393505@qq.com>
2018-11-26 09:22:45 +0800

    vue-cli3.0

---
+++ project/familyE/public/hellovue3.0/precache-manifest.a972be6a0d1ccb03a3ec214c5bbb1d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "859eacc480f25650a49f",
+    "url": "/js/chunk-vendors-legacy.272e5479.js"
+  },
+  {
+    "revision": "d648d9a3e7b60be68c8c",
+    "url": "/js/app-legacy.47519eac.js"
+  },
+  {
+    "revision": "e6f8897ae0ef8e5387e8",
+    "url": "/js/about-legacy.0bf3fb79.js"
+  },
+  {
+    "revision": "b71b8ee5f77f91dfab9b69405f655921",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d648d9a3e7b60be68c8c",
+    "url": "/css/app.890bbcbc.css"
+  }
+];
