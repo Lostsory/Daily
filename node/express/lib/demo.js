a12c65a670cd0c6be0db0536c34c403fb1c1743f
Lostsory <2462393505@qq.com>
2018-12-17 10:18:06 +0800

    test

---
+++ node/express/lib/demo.js
@@ -0,0 +1,7 @@
+/* var express = require('./express')()
+console.log(express.route('./', function(){
+  console.log('2321');
+})); */
+
+var methods = require('methods')
+console.log(Object.keys(methods));
